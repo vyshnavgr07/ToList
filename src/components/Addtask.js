c678ec0787cdb91dcf7d38a5335962684066e33c
Binil B <binil.b.re@gmail.com>
2023-10-24 13:11:43 +0530

    handled null input

--- src/components/Addtask.js
+++ src/components/Addtask.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Addtask = ({addTask}) => {
-    const [value,setvalue]=useState("");
-    const addItem=()=>{
-       addTask(value);
-       setvalue("");
+const Addtask = ({ addTask }) => {
+  const [value, setvalue] = useState("");
+  const addItem = () => {
+    if (value.trim() === "") {
+      toast.error("Please input the task");
+      return; 
     }
+    addTask(value);
+    setvalue("");
+  };
   return (
     <>
-    <div className='input-container'>
-        <input type='text' className='input' placeholder='Add Your Task' 
-        value={value}
-       onChange={(e)=>{setvalue(e.target.value)}}
-       />
-        
-        <Button onClick={addItem} variant="primary"  className='add-btn'>ADD</Button>
-        </div>
-    </>
-  )
-}
+      <div className="input-container">
+        <input
+          type="text"
+          className="input"
+          placeholder="Add Your Task"
+          value={value}
+          onChange={(e) => {
+            setvalue(e.target.value);
+          }}
+        />
 
-export default Addtask
+        <Button onClick={addItem} variant="primary" className="add-btn">
+          ADD
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Addtask;
--- src/components/Listtask.js
+++ src/components/Listtask.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 
-const Listtask = ({task,index,removeTask}) => {
+const Listtask = ({ task, index, removeTask }) => {
   return (
     <>
-        <div className='list-task'>
-           {task.title}
-          <Button onClick={()=>{removeTask(index)}} className='delete-btn'  variant="danger">Remove</Button>
-
-        </div>
+      <div className="list-task">
+        {task.title ? (
+          <>
+            {task.title}
+            <Button
+              onClick={() => {
+                removeTask(index);
+              }}
+              className="delete-btn"
+              variant="danger"
+            >
+              Remove
+            </Button>
+          </>
+        ) : (
+          <div className="empty-task-message">No task title provided</div>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Listtask
+export default Listtask;
